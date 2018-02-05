1240568ad1cf25ed6fee630866282d7b2ec6bac1
ddbrennan <ddbren@gmail.com>
2018-02-05 13:41:56 -0500

    component routes working

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const actorInfo = actors.map(a => {
+    return (
+      <div>
+        <h2>{a.name}</h2>
+        <ul>
+          {a.movies.map((m) => <li>{m}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      { actorInfo }
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const directorInfo = directors.map((d) => {
+    return (
+      <div>
+        <h2>{d.name}</h2>
+        <ul>
+          {d.movies.map((m) => <li>{m}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      { directorInfo }
     </div>
   );
 }
