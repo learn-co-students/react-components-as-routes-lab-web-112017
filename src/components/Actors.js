3ed6269f2a30130f8f51903b0a8ff1a6a750b636
Matt McAlister <matt.mcalister93@gmail.com>
2018-02-06 13:54:29 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { actors } from '../data';
 
+const ActorItem = (props) => {
+  const movies = props.actor.movies.map((m) => <li>{m}</li>);
+
+  return(
+    <div>
+      <h2>Name: {props.actor.name}</h2>
+      <p>Movies:</p>
+      <ul>
+        {movies}
+      </ul>
+    </div>
+  )
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Actors Page</h1>
+        {actors.map(d => <ActorItem key={d.title} actor={d}/>)}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { directors } from '../data';
 
+const DirectorItem = (props) => {
+  const movies = props.director.movies.map((m) => <li>{m}</li>);
+
+  return(
+    <div>
+      <h2>Name: {props.director.name}</h2>
+      <p>Movies:</p>
+      <ul>
+        {movies}
+      </ul>
+    </div>
+  )
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Directors Page</h1>
+        {directors.map(d => <DirectorItem key={d.title} director={d}/>)}
     </div>
   );
-}
+};
 
 export default Directors
