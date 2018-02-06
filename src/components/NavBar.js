c3da64fcc69ff9f25d7f4679910122d6cb68b99e
Flatiron School <leslie.hsu@flatironschool.com>
2018-02-05 22:18:39 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,42 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <ul>
+        <li><NavLink
+          to="/"
+          exact
+          activeStyle={{
+            background: 'yellow'
+          }}
+        >Home</NavLink></li>
+
+        <li><NavLink
+          to="/movies"
+          exact
+          activeStyle={{
+            background: 'yellow'
+          }}
+        >Movies</NavLink></li>
+
+        <li><NavLink
+          to="/directors"
+          exact
+          activeStyle={{
+            background: 'yellow'
+          }}
+        >Directors</NavLink></li>
+
+        <li><NavLink
+          to="/actors"
+          exact
+          activeStyle={{
+            background: 'yellow'
+          }}
+        >Actors</NavLink></li>
+      </ul>
     </div>
-  );
+    );
 };
 
 export default NavBar;
