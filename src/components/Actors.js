4e3caad48bc2bf334c217238963f9246b647b68d
Marvin Clerge <clergemarvin@gmail.com>
2018-02-06 11:17:56 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  function handleActors() {
+    return actors.map(actor => {
+      return(
+        <div>
+          {actor.name}
+          {actor.movies.map(movie => <ul>{movie}</ul>)}
+        </div>
+      )
+    })
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {handleActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  function handleMovies(){
+    return movies.map(movie => {
+      return(
+        <div>
+          {movie.title}
+          {movie.time}
+          {movie.genres.map(genre => <ul>{genre}</ul>)}
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {handleMovies()}
     </div>
   );
 };
