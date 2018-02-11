67ab885fd4ab8a88da86102694e0ac71d275cc2d
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-02-10 19:52:55 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { directors } from '../data';
 
+
+const styles={
+  fontFamily:"droid sans",
+  fontSize:"5em"
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1 style={styles}>Directors Page</h1>
+      {directors.map((d) => {
+        return <div>
+         {d.name}
+        <ul>
+          {d.movies.map((m)=> {
+            return <li>{m}</li>
+          })}
+        </ul>
+      </div>
+      })}
     </div>
   );
 }
