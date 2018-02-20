adcdf3dcad58c8c7c7e2f6298b13834d6ae217ce
nspp99b <chromepanthers@gmail.com>
2018-02-20 10:19:37 -0500

    Configured router, navbar, and all content components

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  
+  const actorsArray = actors.map((a, i) => (
+    <div key={i}>
+      <h3>Name: {a.name}</h3>
+      <h4>Movies:</h4>
+      <ul>
+        {a.movies.map((m, i) => (<li key={i}>{m}</li>))}
+      </ul>
+    </div>
+  ))
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsArray}
     </div>
   );
 };
